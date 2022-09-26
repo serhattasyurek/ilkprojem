db536315cbfae71d5b7bec0ab55ad59fb4969a26
serhattasyurek <serhattasyurek25@gmail.com>
2022-09-26 18:15:32 +0000

    chat programı yaptım

---
+++ robotchat.py
@@ -0,0 +1,19 @@
+cevap=input("selamunaleyküm:")
+if cevap=="aleykümselam":
+    cevap=input("nasılsın?:")
+if cevap=="iyiyim":
+    print("Allah iyilik versin :)")
+if cevap=="kötüyüm":
+    print("üzüldüm :(")
+cevap=input("sence bu sene süper ligde kim şampiyon olur?:")
+if cevap=="galatasaray":
+    cevap=input("olabilir")
+if cevap=="fenerbahçe":
+    cevap=input("olabilir")
+if cevap=="trabzonspor":
+    cevap=input("sanmam")
+if cevap=="beşiktaş":
+    cevap=input("bilmiyorum")
+if cevap=="bilmiyorum":
+    cevap=input("tamam görüşürüz")
+cevap=input("görüşürüz")
